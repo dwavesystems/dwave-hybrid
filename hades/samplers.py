6a10d8545abd2b99f8a4efa025f1b11cbc07dc74
Radomir Stevanovic <radomir@dwavesys.com>
2018-08-02 20:39:31 -0700

    Initial implementation of pipeline async samplers

---
+++ hades/samplers.py
@@ -0,0 +1,130 @@
+from collections import namedtuple
+
+import numpy as np
+import networkx as nx
+
+import dimod
+from dimod import ExactSolver
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+import minorminer
+
+# TODO: pip-ify
+from tabu_sampler import TabuSampler
+
+from hades.utils import (
+    bqm_induced_by, select_localsearch_adversaries,
+    updated_sample, sample_dict_to_list)
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+# TODO: abstract as singleton executor under hades namespace
+from concurrent.futures import ThreadPoolExecutor
+executor = ThreadPoolExecutor(max_workers=2)
+
+
+# tentative container of (sample, energy, ...) tuple
+Solution = namedtuple('Solution', 'sample energy')
+
+
+class QPUSubproblemSampler(object):
+
+    def __init__(self, bqm, max_n, num_reads=100):
+        self.bqm = bqm
+        self.max_n = max_n
+        self.num_reads = num_reads
+
+        self.graph = nx.Graph(bqm.adj)
+        logger.info("Problem graph connected? %r", nx.is_connected(self.graph))
+
+        self.sampler = DWaveSampler()
+
+    def _embed(self, variables, sample):
+        subbqm = bqm_induced_by(self.bqm, variables, sample)
+        source_edgelist = list(subbqm.quadratic) + [(v, v) for v in subbqm.linear]
+        _, target_edgelist, target_adjacency = self.sampler.structure
+        embedding = minorminer.find_embedding(source_edgelist, target_edgelist)
+        bqm_embedded = dimod.embed_bqm(subbqm, embedding, target_adjacency, chain_strength=1.0)
+        return embedding, bqm_embedded, subbqm
+
+    def _run(self, sample):
+        """Finds a subproblem to send to QPU, solves it, and returns a proposed
+        candidate for new global solution (to replace ``sample``).
+        """
+
+        # simple strategy to select high-penalty variables (around sample)
+        # Beware: variables might NOT be connected. For better results, QPU-sample connected vars
+        frozen = select_localsearch_adversaries(self.bqm, sample, self.max_n, min_gain=0)
+
+        # inspect subgraph connectivity before embedding
+        subgraph = nx.Graph(self.graph.subgraph(frozen))
+        logger.info("Subgraph (order %d) connected? %r", subgraph.order(), nx.is_connected(subgraph))
+
+        # embed
+        embedding, bqm_embedded, subbqm = self._embed(frozen, sample)
+
+        # sample with qpu
+        target_response = self.sampler.sample(bqm_embedded, num_reads=self.num_reads)
+
+        # unembed
+        response = dimod.unembed_response(target_response, embedding, subbqm)
+        response_datum = next(response.data())
+        best_sub_sample = response_datum.sample
+
+        composed_sample = updated_sample(sample, best_sub_sample)
+        return Solution(composed_sample, self.bqm.energy(composed_sample))
+
+    def run(self, sample):
+        return executor.submit(self._run, sample)
+
+
+class TabuSubproblemSampler(object):
+
+    def __init__(self, bqm, max_n, num_reads=1, tenure=None, timeout=20):
+        self.bqm = bqm
+        self.max_n = max_n
+        self.num_reads = num_reads
+        self.tenure = tenure
+        self.timeout = timeout
+        self.sampler = TabuSampler()
+
+    def _run(self, sample):
+        # shared
+        frozen = select_localsearch_adversaries(self.bqm, sample, self.max_n, min_gain=0)
+
+        subbqm = bqm_induced_by(self.bqm, frozen, sample)
+        response = self.sampler.sample(
+            subbqm, tenure=self.tenure, timeout=self.timeout, num_reads=self.num_reads)
+        best_sub_sample = next(response.samples())
+        # TODO: use .data(), simplify update
+
+        # shared
+        composed_sample = updated_sample(sample, best_sub_sample)
+        return Solution(composed_sample, self.bqm.energy(composed_sample))
+
+    def run(self, sample):
+        return executor.submit(self._run, sample)
+
+
+class TabuProblemSampler(object):
+
+    def __init__(self, bqm, num_reads=1, tenure=None, timeout=20):
+        self.bqm = bqm
+        self.num_reads = num_reads
+        self.tenure = tenure
+        self.timeout = timeout
+        self.sampler = TabuSampler()
+
+    def _run(self, sample):
+        response = self.sampler.sample(
+            self.bqm, init_solution=sample, tenure=self.tenure,
+            timeout=self.timeout, num_reads=self.num_reads)
+        response_datum = next(response.data())
+        best_sample = sample_dict_to_list(response_datum.sample)
+        best_energy = response_datum.energy
+        return Solution(best_sample, best_energy)
+
+    def run(self, sample):
+        return executor.submit(self._run, sample)
