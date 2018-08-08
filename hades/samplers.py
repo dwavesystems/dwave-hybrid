561e6220c8041e27e3c4f2242c5eea564c6dd3d0
Radomir Stevanovic <radomir@dwavesys.com>
2018-08-07 18:36:35 -0700

    Refactor samplers to extend Runnables

--- hades/samplers.py
+++ hades/samplers.py
@@ -14,30 +14,16 @@ import minorminer
 # TODO: pip-ify
 from tabu_sampler import TabuSampler
 
+from hades.core import executor, Runnable, BranchState
 from hades.utils import (
-    bqm_induced_by, select_localsearch_adversaries,
+    bqm_induced_by, select_localsearch_adversaries, select_random_subgraph,
     updated_sample, sample_dict_to_list)
 
 import logging
 logger = logging.getLogger(__name__)
 
 
-# TODO: abstract as singleton executor under hades namespace
-from concurrent.futures import ThreadPoolExecutor
-executor = ThreadPoolExecutor(max_workers=2)
-
-
-# tentative container of (sample, energy, ...) tuple
-_Solution = namedtuple('Solution', 'sample energy source meta')
-
-class Solution(_Solution):
-    """Identical to _Solution namedtuple, with added default values/kwargs."""
-
-    def __new__(_cls, sample=None, energy=None, source=None, meta=None):
-        return _Solution.__new__(_cls, sample, energy, source, meta)
-
-
-class QPUSubproblemSampler(object):
+class QPUSubproblemSampler(Runnable):
 
     def __init__(self, bqm, max_n, num_reads=100):
         self.name = self.__class__.__name__
@@ -58,14 +44,16 @@ class QPUSubproblemSampler(object):
         bqm_embedded = dimod.embed_bqm(subbqm, embedding, target_adjacency, chain_strength=1.0)
         return embedding, bqm_embedded, subbqm
 
-    def _run(self, sample):
+    def iterate(self, state):
         """Finds a subproblem to send to QPU, solves it, and returns a proposed
         candidate for new global solution (to replace ``sample``).
         """
+        sample = state.sample
 
         # simple strategy to select high-penalty variables (around sample)
         # Beware: variables might NOT be connected. For better results, QPU-sample connected vars
-        frozen = select_localsearch_adversaries(self.bqm, sample, self.max_n, min_gain=0)
+        #frozen = select_localsearch_adversaries(self.bqm, sample, self.max_n, min_gain=0)
+        frozen = select_random_subgraph(self.bqm, self.max_n)
 
         # inspect subgraph connectivity before embedding
         subgraph = nx.Graph(self.graph.subgraph(frozen))
@@ -83,17 +71,10 @@ class QPUSubproblemSampler(object):
         best_sub_sample = response_datum.sample
 
         composed_sample = updated_sample(sample, best_sub_sample)
-        return Solution(composed_sample, self.bqm.energy(composed_sample), self.__class__.__name__)
-
-    def run(self, sample):
-        return executor.submit(self._run, sample)
-
-    def stop(self):
-        # TODO
-        pass
+        return BranchState(composed_sample, self.bqm.energy(composed_sample), self.__class__.__name__)
 
 
-class TabuSubproblemSampler(object):
+class TabuSubproblemSampler(Runnable):
 
     def __init__(self, bqm, max_n, num_reads=1, tenure=None, timeout=20):
         self.name = self.__class__.__name__
@@ -104,9 +85,12 @@ class TabuSubproblemSampler(object):
         self.timeout = timeout
         self.sampler = TabuSampler()
 
-    def _run(self, sample):
+    def iterate(self, state):
+        sample = state.sample
+
         # shared
-        frozen = select_localsearch_adversaries(self.bqm, sample, self.max_n, min_gain=0)
+        #frozen = select_localsearch_adversaries(self.bqm, sample, self.max_n, min_gain=0)
+        frozen = select_random_subgraph(self.bqm, self.max_n)
 
         subbqm = bqm_induced_by(self.bqm, frozen, sample)
         response = self.sampler.sample(
@@ -116,17 +100,10 @@ class TabuSubproblemSampler(object):
 
         # shared
         composed_sample = updated_sample(sample, best_sub_sample)
-        return Solution(composed_sample, self.bqm.energy(composed_sample), self.name)
-
-    def run(self, sample):
-        return executor.submit(self._run, sample)
-
-    def stop(self):
-        # TODO
-        pass
+        return BranchState(composed_sample, self.bqm.energy(composed_sample), self.name)
 
 
-class TabuProblemSampler(object):
+class TabuProblemSampler(Runnable):
 
     def __init__(self, bqm, num_reads=1, tenure=None, timeout=20):
         self.name = self.__class__.__name__
@@ -136,21 +113,15 @@ class TabuProblemSampler(object):
         self.timeout = timeout
         self.sampler = TabuSampler()
 
-    def _run(self, sample):
+    def iterate(self, state):
+        sample = state.sample
         response = self.sampler.sample(
             self.bqm, init_solution=sample, tenure=self.tenure,
             timeout=self.timeout, num_reads=self.num_reads)
         response_datum = next(response.data())
         best_sample = sample_dict_to_list(response_datum.sample)
         best_energy = response_datum.energy
-        return Solution(best_sample, best_energy, self.name)
-
-    def run(self, sample):
-        return executor.submit(self._run, sample)
-
-    def stop(self):
-        # TODO
-        pass
+        return BranchState(best_sample, best_energy, self.name)
 
 
 class InterruptableTabuSampler(TabuProblemSampler):
@@ -162,22 +133,21 @@ class InterruptableTabuSampler(TabuProblemSampler):
         self.max_timeout = timeout
         self._stop_event = threading.Event()
 
-    def _interruptable_run(self, sample):
-        solution = Solution(sample, 0, self.name)
+    def _interruptable_run(self, state):
         start = time.time()
         iterno = 1
         while True:
-            solution = self._run(solution.sample)
+            state = self.iterate(state)
             runtime = time.time() - start
             timeout = self.max_timeout is not None and runtime >= self.max_timeout
             if self._stop_event.is_set() or timeout:
                 break
             iterno += 1
-        return solution._replace(meta=dict(runtime=runtime, iterno=iterno))
+        return state._replace(meta=dict(runtime=runtime, iterno=iterno))
 
-    def run(self, sample):
+    def run(self, state):
         self._stop_event.clear()
-        return executor.submit(self._interruptable_run, sample)
+        return executor.submit(self._interruptable_run, state)
 
     def stop(self):
         self._stop_event.set()
