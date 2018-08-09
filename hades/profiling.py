16f6473809b9de15f57ca5e386a5d75138a8694f
Radomir Stevanovic <radomir@dwavesys.com>
2018-08-08 20:05:25 -0700

    Add context manager support to tictoc

--- hades/profiling.py
+++ hades/profiling.py
@@ -15,6 +15,15 @@ except AttributeError:
 class tictoc(object):
     """Instrument and log function execution duration."""
 
+    def start(self):
+        self.tick = perf_counter()
+        logger.log(self.loglevel, "tic(%r)", self.name, extra={"tic": self.name})
+
+    def stop(self):
+        dt = perf_counter() - self.tick
+        logger.log(self.loglevel, "toc(%r) = %r", self.name, dt,
+                   extra={"toc": self.name, "duration": dt})
+
     def __init__(self, name=None, loglevel=logging.DEBUG):
         self.name = name
         self.loglevel = loglevel
@@ -25,14 +34,17 @@ class tictoc(object):
 
         @functools.wraps(fn)
         def wrapper(*args, **kwargs):
-            start = perf_counter()
-            logger.log(self.loglevel, "tic(%r)", self.name, extra={"tic": self.name})
+            self.start()
             try:
                 return fn(*args, **kwargs)
             finally:
-                end = perf_counter()
-                dt = end - start
-                logger.log(self.loglevel, "toc(%r) = %r", self.name, dt,
-                           extra={"toc": self.name, "duration": dt})
+                self.stop()
 
         return wrapper
+
+    def __enter__(self, name=None, loglevel=logging.DEBUG):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stop()
