06be0521947e2049c9d4543e1ad2b790b28019a3
Radomir Stevanovic <radomir@dwavesys.com>
2018-08-10 16:02:25 -0700

    Add simple decomposers (energy impact, random)

---
+++ hades/decomposers.py
@@ -0,0 +1,38 @@
+from hades.core import Runnable, State
+from hades.utils import (
+    bqm_induced_by, select_localsearch_adversaries, select_random_subgraph)
+
+
+class EnergyImpactDecomposer(Runnable):
+    """Selects up to `max_size` variables that contribute the most to energy
+    increase.
+
+    Note: currently, list of variables not connected in problem graph might be
+    returned.
+    """
+
+    def __init__(self, bqm, max_size, min_gain=0.0):
+        self.bqm = bqm
+        self.max_size = max_size
+        self.min_gain = min_gain
+
+    def iterate(self, state):
+        variables = select_localsearch_adversaries(
+            self.bqm, state.sample.values, self.max_size, min_gain=self.min_gain)
+        subbqm = bqm_induced_by(self.bqm, variables, state.sample.values)
+        return state.updated(ctx=dict(subproblem=subbqm))
+
+
+class RandomSubproblemDecomposer(Runnable):
+    """Selects a random subproblem of size `size`. The subproblem is possibly
+    not connected.
+    """
+
+    def __init__(self, bqm, size):
+        self.bqm = bqm
+        self.size = size
+
+    def iterate(self, state):
+        variables = select_random_subgraph(self.bqm, self.size)
+        subbqm = bqm_induced_by(self.bqm, variables, state.sample.values)
+        return state.updated(ctx=dict(subproblem=subbqm))
