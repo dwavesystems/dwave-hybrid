8937f21097132e6b99a6cd57f1e2ee85847ebec0
Radomir Stevanovic <radomir@dwavesys.com>
2018-08-13 12:28:45 -0700

    Fix SplatComposer, replacement data is dict

--- hades/composers.py
+++ hades/composers.py
@@ -1,6 +1,9 @@
 from hades.core import Runnable, State, Sample
 from hades.utils import updated_sample
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 class SplatComposer(Runnable):
     """Copies subsample values over sample.
@@ -13,6 +16,6 @@ class SplatComposer(Runnable):
         self.bqm = bqm
 
     def iterate(self, state):
-        composed_sample = updated_sample(state.sample.values, state.ctx['subsample'].values)
+        composed_sample = updated_sample(state.sample.values, state.ctx['subsample'])
         composed_energy = self.bqm.energy(composed_sample)
         return state.updated(sample=Sample(composed_sample, composed_energy))
