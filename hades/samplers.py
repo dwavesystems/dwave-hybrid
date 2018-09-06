2ca9582dc679fff9332f737cb284c255225d7ac6
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2018-09-06 08:40:20 -0700

    Update RandomSubproblemSampler

--- hades/samplers.py
+++ hades/samplers.py
@@ -393,6 +393,37 @@ class InterruptableTabuSampler(TabuProblemSampler):
 
 
 class RandomSubproblemSampler(Runnable):
+    """A random sample generator for a subproblem.
+
+    Examples:
+        This example works on a binary quadratic model of two AND gates in series
+        by sampling a BQM representing just one of the gates. Output :math:`z` of gate
+        :math:`z = x \wedge y` connects to input :math:`a` of gate :math:`c = a \wedge b`.
+        An initial state is manually set with invalid solution :math:`x=y=0, z=1; a=b=1, c=0`.
+        The state is updated with a random sample..
+
+        >>> import dimod
+        >>> # Define a problem and a subproblem
+        >>> bqm = dimod.BinaryQuadraticModel({'x': 0.0, 'y': 0.0, 'z': 8.0, 'a': 2.0, 'b': 0.0, 'c': 6.0},
+        ...                                  {('y', 'x'): 2.0, ('z', 'x'): -4.0, ('z', 'y'): -4.0,
+        ...                                  ('b', 'a'): 2.0, ('c', 'a'): -4.0, ('c', 'b'): -4.0, ('a', 'z'): -4.0},
+        ...                                  -1.0, 'BINARY')
+        >>> sub_bqm = dimod.BinaryQuadraticModel({'x': 0.0, 'y': 0.0, 'z': 8.0},
+        ...                                      {('x', 'y'): 2.0, ('x', 'z'): -4.0, ('y', 'z'): -4.0},
+        ...                                      -1.0, dimod.Vartype.BINARY)
+        >>> # Set up the sampler with an initial state
+        >>> sampler = samplers.RandomSubproblemSampler()
+        >>> state = core.State().from_sample({'x': 0, 'y': 0, 'z': 1, 'a': 1, 'b': 1, 'c': 0}, bqm)
+        >>> state.ctx.update(subproblem=sub_bqm)
+        >>> # Sample the subproblem a couple of times
+        >>> new_state = sampler.iterate(state)
+        >>> print(new_state.ctx['subsamples'].record)      # doctest: +SKIP
+        [([0, 0, 0], -1., 1)]
+        >>> new_state = sampler.iterate(state)
+        >>> print(new_state.ctx['subsamples'].record)      # doctest: +SKIP
+        [([1, 1, 1], 1., 1)]
+
+    """
 
     @tictoc('random_sample')
     def iterate(self, state):
