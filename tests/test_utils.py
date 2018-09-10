c29642d17fb9b907a59a3ab6d0390d66b848303a
Radomir Stevanovic <radomir@dwavesys.com>
2018-09-10 16:39:29 -0700

    Add test for flip energy gain calc util

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,10 +3,38 @@ import unittest
 import dimod
 import dwave_networkx as dnx
 
-from hades.utils import chimera_tiles
+from hades.utils import chimera_tiles, flip_energy_gains
 
 
-class Test_chimera_tiles(unittest.TestCase):
+class TestEnergyFlipGainUtils(unittest.TestCase):
+    # minimized when variables differ
+    notall = dimod.BinaryQuadraticModel({}, {'ab': 1, 'bc': 1, 'ca': 1}, 0, dimod.SPIN)
+
+    # minimized when B is different from A and C
+    notb = dimod.BinaryQuadraticModel({}, {'ab': 1, 'bc': 1, 'ca': -1}, 0, dimod.SPIN)
+
+    def test_asc(self):
+        # flipping C makes for the highest energy gain
+        gains = flip_energy_gains(self.notall, {'a': 1, 'b': 1, 'c': -1})
+        self.assertEqual(gains, [(4.0, 'c'), (0.0, 'b'), (0.0, 'a')])
+        gains = flip_energy_gains(self.notall, {'a': -1, 'b': -1, 'c': 1})
+        self.assertEqual(gains, [(4.0, 'c'), (0.0, 'b'), (0.0, 'a')])
+
+        # flipping any is equally bad
+        gains = flip_energy_gains(self.notb, {'a': 1, 'b': -1, 'c': 1})
+        self.assertEqual(gains, [(4.0, 'c'), (4.0, 'b'), (4.0, 'a')])
+
+    def test_desc(self):
+        # flipping any is equally good
+        gains = flip_energy_gains(self.notall, {'a': 1, 'b': 1, 'c': 1})
+        self.assertEqual(gains, [(-4.0, 'c'), (-4.0, 'b'), (-4.0, 'a')])
+
+        # flipping B is the worst
+        gains = flip_energy_gains(self.notb, {'a': 1, 'b': 1, 'c': 1})
+        self.assertEqual(gains, [(0.0, 'c'), (0.0, 'a'), (-4.0, 'b')])
+
+
+class TestChimeraTiles(unittest.TestCase):
     def test_single_target(self):
         bqm = dimod.BinaryQuadraticModel.from_qubo({edge: 1 for edge in dnx.chimera_graph(4).edges})
 
