14b83a6acbdf660201fa0dbf2cd24302c0ddff00
Radomir Stevanovic <radomir@dwavesys.com>
2018-10-12 12:10:09 -0700

    Move canonical_chimera_labeling to dwave-networkx

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except SyntaxError:
 
 # Package requirements, minimal pinning
 install_requires = ['six>=1.10', 'numpy', 'networkx', 'click>5', 'plucky>=0.4.3',
-                    'dimod>=0.7.5', 'minorminer', 'dwave-system', 'dwave-networkx',
+                    'dimod>=0.7.5', 'minorminer', 'dwave-system', 'dwave-networkx>=0.6.6',
                     'dwave-neal>=0.4.1', 'dwave-tabu']
 
 # Package extras requirements
