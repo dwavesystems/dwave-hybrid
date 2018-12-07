21abde20aeef747a990f022135c5dd166a06d5f6
Radomir Stevanovic <radomir@dwavesys.com>
2018-12-06 16:34:45 -0800

    Test composed (flow control) blocks' traits

--- tests/test_traits.py
+++ tests/test_traits.py
@@ -17,6 +17,7 @@ import unittest
 import dimod
 
 from hybrid.core import State, States, Runnable, Branch
+from hybrid.flow import RacingBranches, Map
 from hybrid.utils import min_sample
 from hybrid import traits
 
@@ -175,3 +176,101 @@ class TestMultipleStateTraits(unittest.TestCase):
 
         with self.assertRaises(traits.StateDimensionalityError):
             InvalidMIMO().run(States()).result()
+
+    def test_all_state_traits_enforced(self):
+        class A(Runnable, traits.SIMO, traits.SubproblemIntaking, traits.EmbeddingProducing):
+            def next(self, state):
+                # FAIL: embedding is missing in second state
+                return States(State(embedding=1), State())
+
+        with self.assertRaises(traits.StateTraitMissingError):
+            A().run(State()).result()
+
+        with self.assertRaises(traits.StateTraitMissingError):
+            A().run(State(subproblem=1)).result()
+
+        class B(Runnable, traits.SIMO, traits.SubproblemIntaking, traits.EmbeddingProducing):
+            def next(self, state):
+                return States(State(embedding=1), State(embedding=2))
+
+        with self.assertRaises(traits.StateTraitMissingError):
+            # subproblem missing on input
+            B().run(State()).result()
+
+        self.assertEqual(len(B().run(State(subproblem=1)).result()), 2)
+
+
+class TestFlowComponentsTraits(unittest.TestCase):
+
+    def test_branch_with_single_component(self):
+        """Traits requirements from inner runnable must be reflected in branch."""
+        class ValidSISO(Runnable, traits.SISO):
+            def next(self, state):
+                return state
+
+        with self.assertRaises(traits.StateDimensionalityError):
+            Branch(components=(ValidSISO(),)).run(States()).result()
+        self.assertEqual(Branch(components=(ValidSISO(),)).run(State(x=1)).result().x, 1)
+
+        class InvalidSISO(Runnable, traits.SISO):
+            def next(self, state):
+                return States(state, state)
+
+        with self.assertRaises(traits.StateDimensionalityError):
+            Branch(components=(InvalidSISO(),)).run(State()).result()
+            Branch(components=(InvalidSISO(),)).run(States()).result()
+
+        # input: list of states with subproblem
+        # output: list of states with subsamples
+        class SubproblemSamplerMIMO(Runnable, traits.MIMO, traits.SubproblemSampler):
+            def next(self, states):
+                return States(State(subsamples=1), State(subsamples=2))
+
+        with self.assertRaises(traits.StateDimensionalityError):
+            Branch(components=(SubproblemSamplerMIMO(),)).run(State()).result()
+        with self.assertRaises(traits.StateTraitMissingError):
+            Branch(components=(SubproblemSamplerMIMO(),)).run(States(State())).result()
+
+        r = Branch(components=(SubproblemSamplerMIMO(),)).run(States(State(subproblem=True))).result()
+        self.assertEqual(r[0].subsamples, 1)
+        self.assertEqual(r[1].subsamples, 2)
+
+    def test_branch_with_multiple_components(self):
+        class A(Runnable, traits.ProblemDecomposer):
+            def next(self, state):
+                return state.updated(subproblem=state.problem)
+
+        class B(Runnable, traits.SubproblemSampler):
+            def next(self, state):
+                return state.updated(subsamples=state.subproblem)
+
+        a, b = A(), B()
+        branch = a | b
+        self.assertEqual(branch.run(State(problem=1)).result().subsamples, 1)
+        self.assertSetEqual(branch.inputs, a.inputs)
+        self.assertSetEqual(branch.outputs, b.outputs)
+
+    def test_racing_branches(self):
+        class A(Runnable, traits.ProblemDecomposer):
+            def next(self, state):
+                return state.updated(subproblem=state.problem)
+
+        class B(Runnable, traits.SubproblemSampler):
+            def next(self, state):
+                return state.updated(subsamples=state.subproblem)
+
+        a, b = A(), B()
+        race = RacingBranches(a, b)
+        self.assertSetEqual(race.inputs, a.inputs | b.inputs)
+        self.assertSetEqual(race.outputs, a.outputs & b.outputs)
+
+    def test_map(self):
+        class A(Runnable, traits.ProblemDecomposer):
+            def next(self, state):
+                return state.updated(subproblem=state.problem)
+
+        a = A()
+        m = Map(a)
+        r = m.run(States(State(problem=1))).result()
+        self.assertSetEqual(m.inputs, a.inputs)
+        self.assertSetEqual(m.outputs, a.outputs)
