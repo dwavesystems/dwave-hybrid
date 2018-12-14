e845430ae9cd56fda6e63f1bcaf840bf1bc59e79
Radomir Stevanovic <radomir@dwavesys.com>
2018-12-14 10:32:00 -0800

    Test structured child sampler support in QPU auto emb sampler

--- tests/test_samplers.py
+++ tests/test_samplers.py
@@ -16,6 +16,7 @@ import unittest
 
 import dimod
 from neal import SimulatedAnnealingSampler
+from dwave.system.testing import MockDWaveSampler
 
 from hybrid.samplers import *
 from hybrid.core import State
@@ -25,10 +26,24 @@ class TestQPUSamplers(unittest.TestCase):
 
     def test_unstructured_child_sampler(self):
         q = QPUSubproblemAutoEmbeddingSampler(qpu_sampler=SimulatedAnnealingSampler())
+
+        # test sampler stays unstructured
         self.assertFalse(isinstance(q.sampler, dimod.Structured))
 
+        # test sampling works
         bqm = dimod.BinaryQuadraticModel({'a': 1}, {}, 0, 'SPIN')
         init = State.from_subsample({'a': 1}, bqm)
         res = q.run(init).result()
+        self.assertEqual(res.subsamples.first.energy, -1)
 
+    def test_structured_child_sampler(self):
+        q = QPUSubproblemAutoEmbeddingSampler(qpu_sampler=MockDWaveSampler())
+
+        # test sampler is converted to unstructured
+        self.assertFalse(isinstance(q.sampler, dimod.Structured))
+
+        # test sampling works
+        bqm = dimod.BinaryQuadraticModel({'a': 1}, {}, 0, 'SPIN')
+        init = State.from_subsample({'a': 1}, bqm)
+        res = q.run(init).result()
         self.assertEqual(res.subsamples.first.energy, -1)
