0021388377849329666c059aec983c19b5fde591
Radomir Stevanovic <radomir@dwavesys.com>
2019-01-25 12:38:40 -0800

    Add alternative qbsolv impl example: qpu-sample 15% of problem

---
+++ examples/qbsolv-like-alt.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+# Copyright 2018 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+import sys
+
+import dimod
+import hybrid
+
+
+# load a problem
+problem = sys.argv[1]
+with open(problem) as fp:
+    bqm = dimod.BinaryQuadraticModel.from_coo(fp)
+
+# define the solver
+def merge_substates(component, substates):
+    a, b = substates
+    return a.updated(
+        subsamples=hybrid.utils.meld_samplesets(a.subsamples, b.subsamples)
+    )
+
+subsampler = hybrid.Unwind(
+    hybrid.EnergyImpactDecomposer(size=50, silent_rewind=False, rolling_history=0.15)
+) | hybrid.Map(
+    hybrid.QPUSubproblemAutoEmbeddingSampler()
+) | hybrid.Reduce(
+    hybrid.Lambda(merge_substates)
+) | hybrid.SplatComposer()
+
+iteration = hybrid.Race(
+    hybrid.InterruptableTabuSampler(),
+    subsampler 
+) | hybrid.ArgMin()
+
+main = hybrid.Loop(iteration, max_iter=10, convergence=3)
+
+# run solver
+init_state = hybrid.State.from_sample(hybrid.min_sample(bqm), bqm)
+solution = main.run(init_state).result()
+
+# show execution profile
+hybrid.profiling.print_counters(main)
+
+# show results
+print("Solution: sample={s.samples.first}".format(s=solution))
