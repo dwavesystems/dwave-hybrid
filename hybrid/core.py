1baf30033fd9f52b57a34e87b94af48bd4d76ce6
Radomir Stevanovic <radomir@dwavesys.com>
2019-01-25 21:26:37 -0800

    Bugfix States: broadcast .updated to all states

--- hybrid/core.py
+++ hybrid/core.py
@@ -221,6 +221,10 @@ class States(list):
     def result(self):
         return self
 
+    def updated(self, **kwargs):
+        """Return a (deep) copy of the states, updated from `kwargs`."""
+        return States(state.updated(**kwargs) for state in self)
+
 
 class Present(Future):
     """Already resolved :class:`~concurrent.futures.Future` object.
