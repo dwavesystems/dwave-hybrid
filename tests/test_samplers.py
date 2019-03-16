fd9a8a89f7b889f69a1c22239ec214109389124e
Radomir Stevanovic <radomir@dwavesys.com>
2019-03-15 17:27:42 -0700

    Test tabu samplers

--- tests/test_samplers.py
+++ tests/test_samplers.py
@@ -14,6 +14,7 @@
 
 import unittest
 
+import time
 import dimod
 from neal import SimulatedAnnealingSampler
 from dwave.system.testing import MockDWaveSampler
@@ -75,3 +76,56 @@ class TestQPUSamplers(unittest.TestCase):
         self.assertEqual(res.subsamples.first.energy, -1)
         self.assertEqual(sampler.initial_state.popitem()[1], 1)
         self.assertEqual(sampler.anneal_schedule, ra.anneal_schedule)
+
+
+class TestTabuSamplers(unittest.TestCase):
+
+    def test_tabu_problem_sampler_interface(self):
+        bqm = dimod.BinaryQuadraticModel({'a': 1}, {}, 0, 'SPIN')
+
+        workflow = TabuProblemSampler(num_reads=10)
+
+        init = State.from_sample({'a': 1}, bqm)
+        result = workflow.run(init).result()
+
+        self.assertEqual(result.samples.first.energy, -1)
+        self.assertEqual(len(result.samples), 10)
+
+    def test_tabu_problem_sampler_functionality(self):
+        bqm = dimod.BinaryQuadraticModel({}, {'ab': 1, 'bc': -1, 'ca': 1}, 0, 'SPIN')
+
+        workflow = TabuProblemSampler()
+
+        # use random sample as initial value
+        init = State(problem=bqm, samples=None)
+        result = workflow.run(init).result()
+
+        self.assertEqual(result.samples.first.energy, -3)
+        self.assertEqual(len(result.samples), 1)
+
+    def test_tabu_subproblem_sampler(self):
+        bqm = dimod.BinaryQuadraticModel({}, {'ab': 1, 'bc': -1, 'ca': 1}, 0, 'SPIN')
+
+        workflow = TabuSubproblemSampler()
+
+        # use random sample as initial value
+        init = State(subproblem=bqm, subsamples=None)
+        result = workflow.run(init).result()
+
+        self.assertEqual(result.subsamples.first.energy, -3)
+        self.assertEqual(len(result.subsamples), 1)
+
+    def test_interruptable_tabu(self):
+        bqm = dimod.BinaryQuadraticModel({}, {'ab': 1, 'bc': -1, 'ca': 1}, 0, 'SPIN')
+
+        workflow = InterruptableTabuSampler()
+
+        init = State(problem=bqm)
+        future = workflow.run(init)
+        while len(workflow.runnable.timers.get('dispatch.next', ())) < 1:
+            time.sleep(0)
+
+        workflow.stop()
+
+        self.assertEqual(future.result().samples.first.energy, -3)
+        self.assertGreater(len(workflow.timers['dispatch.next']), 0)
