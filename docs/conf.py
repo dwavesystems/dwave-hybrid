e9f2f1757a16f4d82ca2ce5908193171b3212904
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2019-05-28 05:46:39 -0700

    Implement Radomir's changes

--- docs/conf.py
+++ docs/conf.py
@@ -96,6 +96,7 @@ from hybrid.flow import *
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ['_static']
 
 def setup(app):
    app.add_stylesheet('cookie_notice.css')
