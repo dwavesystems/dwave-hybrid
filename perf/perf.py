e1274298a787f716f1bc3cdca59d46575ced1a11
Radomir Stevanovic <radomir@dwavesys.com>
2019-06-11 17:44:58 -0700

    Refactor of perf script (forward-ported from v0.1.0-benchmark)
    - run workflows and samplers
    - include all problems (up to N per group)
    - support time-to-target run

--- perf/perf.py
+++ perf/perf.py
@@ -16,6 +16,7 @@
 
 """Performance tests."""
 
+import os
 import sys
 import json
 from itertools import chain
@@ -29,34 +30,30 @@ from dwave.system import DWaveSampler, EmbeddingComposite
 from dwave_qbsolv import QBSolv
 
 
-class QBSolvProblemSampler(hybrid.Runnable):
-    """QBSolv wrapper for hybrid."""
+problems_per_group = 10
 
-    def __init__(self, qpu_sampler=None):
-        super(QBSolvProblemSampler, self).__init__()
-        self.sampler = qpu_sampler
+problems = list(chain(
+    sorted(glob('../problems/qbsolv/bqp50_*'))[:problems_per_group],
+    sorted(glob('../problems/qbsolv/bqp100_*'))[:problems_per_group],
+    sorted(glob('../problems/qbsolv/bqp250_*'))[:problems_per_group],
+    sorted(glob('../problems/qbsolv/bqp500_*'))[:problems_per_group],
+    sorted(glob('../problems/qbsolv/bqp1000_*'))[:problems_per_group],
+    sorted(glob('../problems/qbsolv/bqp2500_*'))[:problems_per_group],
+    sorted(glob('../problems/random-chimera/2048*'))[:problems_per_group],
+    sorted(glob('../problems/random-chimera/8192*'))[:problems_per_group],
+    sorted(glob('../problems/ac3/*'))[:problems_per_group],
+))
 
-    def next(self, state):
-        response = QBSolv().sample(state.problem, solver=self.sampler)
-        return state.updated(samples=response)
-
-
-problems = chain(
-    sorted(glob('problems/qbsolv/bqp100_*'))[:5],
-    sorted(glob('problems/qbsolv/bqp2500_*'))[:5],
-    sorted(glob('problems/random-chimera/2048*'))[:5],
-    sorted(glob('problems/random-chimera/8192*'))[:5],
-    sorted(glob('problems/ac3/*'))[:5],
-)
-
-solver_factories = [
-    ("10 second Tabu",
+workflows = [
+    ("10s-tabu",
         lambda **kw: hybrid.TabuProblemSampler(timeout=10000)),
 
-    ("10k sweeps Simulated Annealing",
-        lambda **kw: hybrid.IdentityDecomposer() | hybrid.SimulatedAnnealingSubproblemSampler(sweeps=10000) | hybrid.SplatComposer()),
+    ("10k-sa",
+        lambda **kw: (hybrid.IdentityDecomposer()
+                      | hybrid.SimulatedAnnealingSubproblemSampler(sweeps=10000)
+                      | hybrid.SplatComposer())),
 
-    ("qbsolv-like solver",
+    ("qbsolv-like",
         lambda qpu, **kw: hybrid.Loop(hybrid.Race(
             hybrid.InterruptableTabuSampler(quantum_timeout=200),
             hybrid.EnergyImpactDecomposer(size=50, rolling=True, rolling_history=0.15)
@@ -64,60 +61,84 @@ solver_factories = [
             | hybrid.SplatComposer()
         ) | hybrid.ArgMin(), max_iter=100, convergence=10)),
 
-    ("tiling chimera solver",
+    ("tiling-chimera",
         lambda qpu, **kw: hybrid.Loop(hybrid.Race(
             hybrid.InterruptableTabuSampler(quantum_timeout=200),
             hybrid.TilingChimeraDecomposer(size=(16,16,4))
             | hybrid.QPUSubproblemExternalEmbeddingSampler(qpu_sampler=qpu)
             | hybrid.SplatComposer(),
         ) | hybrid.ArgMin(), max_iter=100, convergence=10)),
+]
+
+samplers = [
+    ("kerberos",
+        lambda **kw: hybrid.KerberosSampler()),
 
     ("qbsolv-classic",
-        lambda **kw: QBSolvProblemSampler()),
+        lambda **kw: QBSolv()),
 
     ("qbsolv-qpu",
-        lambda qpu, **kw: QBSolvProblemSampler(qpu_sampler=qpu)),
-
+        lambda qpu, **kw: QBSolv(solver=EmbeddingComposite(qpu))),
 ]
 
 
-def run(problems, solver_factories):
+
+def run(problems, workflows, samplers, n_runs=1, targets=None):
     results = OrderedDict()
+    targets = targets or {}
+
+    def workflow_runner(bqm, workflow, **kwargs):
+        init_state = hybrid.State.from_sample(hybrid.min_sample(bqm), bqm)
+        with hybrid.tictoc() as timer:
+            samples = workflow.run(init_state).result().samples
+        return samples, timer
+
+    def sampler_runner(bqm, sampler, energy_threshold=None, **kwargs):
+        with hybrid.tictoc() as timer:
+            samples = sampler.sample(bqm, init_sample=lambda: hybrid.min_sample(bqm),
+                                     energy_threshold=energy_threshold)
+        return samples, timer
 
     # reuse the cloud client
-    qpu = EmbeddingComposite(DWaveSampler())
+    qpu = DWaveSampler()
 
-    for problem in problems:
-        results[problem] = OrderedDict()
+    for problem_filename in problems:
+        problem_basename = os.path.splitext(os.path.basename(problem_filename))[0]
 
-        with open(problem) as fp:
+        target_energy = targets.get(problem_basename)
+        results[problem_basename] = OrderedDict()
+
+        with open(problem_filename) as fp:
             bqm = dimod.BinaryQuadraticModel.from_coo(fp)
 
-        for name, factory in solver_factories:
-            case = '{!r} with {!r}'.format(problem, name)
+        for runner, solvers in [(workflow_runner, workflows), (sampler_runner, samplers)]:
+            for solver_name, factory in solvers:
+                run_results = []
 
-            try:
-                solver = factory(qpu=qpu)
-                init_state = hybrid.State.from_sample(hybrid.min_sample(bqm), bqm)
+                for run in range(n_runs):
+                    case = '{!r} with {!r}, run={!r}, target={!r}'.format(
+                        problem_basename, solver_name, run, target_energy)
 
-                with hybrid.tictoc(case) as timer:
-                    solution = solver.run(init_state).result()
+                    try:
+                        samples, timer = runner(bqm, factory(qpu=qpu),
+                                                energy_threshold=target_energy)
 
-            except Exception as exc:
-                print("FAILED {case}: {exc!r}".format(**locals()))
-                results[problem][name] = repr(exc)
+                    except Exception as exc:
+                        print("FAILED {case}: {exc!r}".format(**locals()))
+                        run_results.append(dict(error=repr(exc)))
 
-            else:
-                print("case={case!r}"
-                      " energy={solution.samples.first.energy!r},"
-                      " wallclock={timer.dt!r}".format(**locals()))
-                results[problem][name] = dict(
-                    energy=solution.samples.first.energy,
-                    wallclock=timer.dt)
+                    else:
+                        print("case={case!r}"
+                            " energy={samples.first.energy!r},"
+                            " wallclock={timer.dt!r}".format(**locals()))
+                        run_results.append(dict(energy=samples.first.energy,
+                                                wallclock=timer.dt))
+
+                results[problem_basename][solver_name] = run_results
 
     return results
 
 
 if __name__ == "__main__":
-    results = run(problems, solver_factories)
+    results = run(problems[:1], workflows[:0], samplers[:1], n_runs=1)
     print(json.dumps(results), file=sys.stderr)
