643926d260482b5b910ede4018cad992532f5fbd
Radomir Stevanovic <radomir@dwavesys.com>
2023-08-24 06:37:40 -0700

    Require latest cloud-client to avoid pydantic 2.0 break

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ install_requires = ['numpy>=1.19.1', 'networkx', 'click>5', 'plucky>=0.4.3',
                     # as it's already constrained by dimod (if required).
                     'dwave-preprocessing',
                     'minorminer>=0.1.7', 'dwave-networkx>=0.8.8', 'dwave-system>=1.13.0',
+                    'dwave-cloud-client>=0.10.6',   # avoid pydantic 2.0 backward compat break
                     'dwave-neal>=0.5.4', 'dwave-tabu>=0.2.0', 'dwave-greedy>=0.1.0']
 
 # Package extras requirements
