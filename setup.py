3c67b6381cc217e591d76df91268c40c0c30c2ef
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2024-11-21 07:08:22 -0800

    Drop py38 support

--- setup.py
+++ setup.py
@@ -28,14 +28,13 @@ extras_require = {
     'test': ['coverage'],
 }
 
-python_requires = ">=3.8"
+python_requires = ">=3.9"
 
 classifiers = [
     'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
     'Development Status :: 3 - Alpha',
     'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
